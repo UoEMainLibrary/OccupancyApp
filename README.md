44208bc5f39797723ee6ee5341c39b0b24984f69
j4bbi <hrafn.malmquist@gmail.com>
2019-04-12 10:44:48 +0100

    Better parametrised, documented

--- README.md
+++ README.md
@@ -2,14 +2,42 @@
 Main Library Occupancy Traffic Light System
 
 Uses [D3](https://github.com/d3/d3) v.4 (not 5!). Designed to be 100px height (ok to change) and will be 90% of width (max 1200px).
-There's a cutoff set to 50% (`var cut_off = .5;`)
+It requires the following parameters being set (default values):
 
-It expects a `MainLibrary.json` with the structure:
+## General
+* `var json_file = 'test.json';` - JSON file containing traffic lights
+* `var cut_off = .5;` - cutoff set to 50%, meaning the ticker will start at 50%
+* `var width = .9';` - width of app, relative. Must be floating point
+* `var max_width = 1200;` - maximum width of app, absolute. Must be pixels.
+* `var height = 100;` - height of app, absolute. Must be pixels.
+* `var tooltip_shift = -50;` - how far south (negative value are north) to shift the tool tip from the pointer
+* `var textual = [];` - an array of objects corresponding to the traffic light feed in the `json_file`, with each object having a `tip` property and a `text` property with textual description of the traffic light. This was separated from the traffic light json so as not to mingle data and text.
+
+## Style
+* `var font_family = "'Source Sans Pro', sans-serif";` - font used
+* `var line_width = 25;` - the height of the traffic light "line"
+* `var font_size = 30;` - font size for the main indicator
+* `var marker_font_size = 15;` - font size for marker
+* `var marker_line_height = 22.5;` - height of tick line
+### Main indicator arrow
+* `var arrow_width = font_size - 2;` - width of arrow indicating point on axis
+* `var arrow_y_shift = 10;` - how far 'south', (negative value are north) to shift said arrow
+* `var arrow_slope = line_width/2;` - the slope of the arrow (how pointy?)
+* `var arrow_color = 'black';` - background color of all arrows
+### Smaller arrows indicating status
+* `var small_arrows = 3;` - how many arrows to indicate status
+* `var small_arrow_width = line_width/2;` - width of small arrow
+* `var small_arrow_height = small_arrow_width/2;` - height of small arrow
+
+It expects a json file set in `json_file` with the structure:
 ```
 {"date":"10/10/2018","time":"17:10","states":[
   {"nr":0,"color":"green","min":null,"max":1610,"selected":true,"level":"decreasing","percent":67},
   {"nr":1,"color":"amber","min":1610,"max":1955},
   {"nr":2,"color":"red","min":1955,"max":2185},
-  {"nr":3,"color":"purple","min":2185,"max":null}]
+  {"nr":3,"color":"purple","min":2185,"max":2300}]
 }
 ```
+
+# TODO
+* Webpack?
--- occupancy_app.js
+++ occupancy_app.js
@@ -1,25 +1,10 @@
 // Library Occupancy App
 // Done by Hrafn Malmquist, Library Digital Development Team
 
-var textual = [
-  { 'text': 'Not busy', 'tip': 'You will find a choice of spaces in the library.' },
-  { 'text': 'Busy', 'tip':'The library is busy. You will find a space but you may not find your preferred seat.'},
-  { 'text': 'Very busy', 'tip': 'The library is very busy. It will be difficult to find a space.'},
-  { 'text': 'Packed!', 'tip': 'There are very few spare spaces left within the library and it will take some time to find a seat.'},]
-
 var tool_tip = null;
 var svg = null;
 var buddy = null;
-var width = '90%';
-var height = 100;
-var cut_off = .5;
-var font_family = "'Source Sans Pro', sans-serif";
-
-var line_width = 25;
-var font_size = 30;
-var marker_font_size = 15;
-var marker_line_height = 22.5;
-var svg_width;
+var svg_width = null;
 
 function getSelected() { // Return selected light
   for(var i = 0; i < traffic_lights.length; i++) {
@@ -28,18 +13,18 @@ function getSelected() { // Return selected light
   }
 }
 
-function showToolTip(id, coords, text) { // Show a tooltip    
+function showToolTip(id, coords, text) { // Show a tooltip
   var tip = (id == selected_obj.nr) ? 'The library is at ' + selected_obj.percent + '% of capacity. ' + text: text; // Explain level of busyness
     tool_tip
     .html(tip)
     .style('visibility', 'visible');
-  
+
   var tooltip_height = tool_tip.node().getBoundingClientRect().height;
   var tooltip_width = tool_tip.node().getBoundingClientRect().width;
 
   var x = Math.max(10, coords[0] - tooltip_width); // Not too far to the left
   var y = Math.max(10, coords[1] - (tooltip_height/2)); // Not too high
-  y = Math.min(coords[1] + tooltip_height, height - tooltip_height - 20); // Not too low
+  y = Math.min(coords[1] + tooltip_height, height - tooltip_height + tooltip_shift); // Not too low
 
   tool_tip
     .style('left', x + 'px')
@@ -71,18 +56,17 @@ function init() {
   createOccupancyApp();
 }
 
-function createOccupancyApp() {  
+function createOccupancyApp() {
   svg.selectAll('*').remove();
 
-  d3.json('MainLibrary.json', function(obj) {
+  d3.json(json_file, function(obj) {
   traffic_lights = obj.states;
-  traffic_lights[traffic_lights.length-1].max = 2300;
-  traffic_lights[0].min = traffic_lights[traffic_lights.length-1].max*cut_off;
+  traffic_lights[0].min = Math.floor(traffic_lights[traffic_lights.length-1].max*cut_off);
   var span = traffic_lights[traffic_lights.length-1].max - traffic_lights[0].min;
   selected_obj = getSelected();
-  
+
   // Let's not have it wider than 1200
-  svg_width = Math.min(document.body.clientWidth*.9, 1200);
+  svg_width = Math.min(document.body.clientWidth*width, max_width);
   buddy.style('width', svg_width + 'px');
   tool_tip.style('width', Math.max(svg_width / 3, 200));
 
@@ -100,9 +84,9 @@ function createOccupancyApp() {
       .attr('x2', function(d) { return this.x1.baseVal.value + (((d.max - d.min)/span)*svg_width); })
       .attr('y2', height - (line_width/2) - marker_line_height)
       .attr('stroke-width', line_width)
-      .style('cursor', 'help')   
+      .style('cursor', 'help')
       .style('stroke', function(d) { return (d.color == 'amber') ? '#FFC200': d.color; });
-  
+
   sub_g.append('line') // Axis line
       .attr('x1', function(d) { return ((d.min - traffic_lights[0].min)/span)*svg_width; })
       .attr('y1', height - marker_line_height)
@@ -112,26 +96,21 @@ function createOccupancyApp() {
       .attr('stroke', 'black');
 
   sub_g.append('text') // Axis label text
-      .text(function(d){ return (d.min/traffic_lights[traffic_lights.length-1].max*100) + "%"; })
+      .text(function(d){ return Math.round(d.min/traffic_lights[traffic_lights.length-1].max*100) + "%"; })
       .attr('alignment-baseline', 'middle')
       .attr('text-anchor', 'middle')
       .style('font-family', font_family)
       .style('font-size', marker_font_size)
-      .style('font-weight', 'bold')      
+      .style('font-weight', 'bold')
       .attr('x', function(d) { return Math.max(((d.min - traffic_lights[0].min)/span)*svg_width, (this.getComputedTextLength()/2)+1); })
       .attr('y', height - (line_width/2) + 5);
 
-  var triangle_height = height - line_width;  
-  var triangle_width = font_size - 2;
   var x_base =  ((traffic_lights[traffic_lights.length-1].max * selected_obj.percent/100-traffic_lights[0].min)/span*svg_width);
-  var y_shift = 10;
-  var y_difference = line_width/2;
 
-
-  var points = '' + (x_base - triangle_width) + ', ' + (height - (line_width*2) - marker_line_height + y_shift) // Left x, y
-  points +=  ', ' + x_base + ', ' + (height - (line_width) - marker_line_height + y_shift) // Middle x, y - bottom
-  points +=  ', ' + (x_base + triangle_width) + ', ' + (height - (line_width*2) - marker_line_height + y_shift) // Right x, y
-  points +=  ', ' + x_base + ', ' + (height - line_width - marker_line_height - y_difference + y_shift) // Low point x, y
+  var points = '' + (x_base - arrow_width) + ', ' + (height - (line_width*2) - marker_line_height + arrow_y_shift) // Left x, y
+  points +=  ', ' + x_base + ', ' + (height - (line_width) - marker_line_height + arrow_y_shift) // Middle x, y - bottom
+  points +=  ', ' + (x_base + arrow_width) + ', ' + (height - (line_width*2) - marker_line_height + arrow_y_shift) // Right x, y
+  points +=  ', ' + x_base + ', ' + (height - line_width - marker_line_height - arrow_slope + arrow_y_shift) // Low point x, y
 
     // Draw the arrow
 
@@ -142,7 +121,7 @@ function createOccupancyApp() {
     // Triangle part of arrow
     arrow.append('polygon')
       .attr('points', points)
-      .attr('fill', 'black')
+      .attr('fill', arrow_color)
       .attr('stroke', 'white')
       .attr('stroke-width', 1)
       .attr('stroke-linecap', 'round')
@@ -151,11 +130,11 @@ function createOccupancyApp() {
     // Line part of arrow
     arrow.append('line')
       .attr('x1', x_base )
-      .attr('y1', height - (line_width) - marker_line_height - 4 + y_shift)
+      .attr('y1', height - (line_width) - marker_line_height - 4 + arrow_y_shift)
       .attr('x2', x_base )
-      .attr('y2', height - (line_width*2) - marker_line_height + y_shift)
-      .attr('stroke', 'black')
-      .attr('stroke-width', y_difference/3);
+      .attr('y2', height - (line_width*2) - marker_line_height + arrow_y_shift)
+      .attr('stroke', arrow_color)
+      .attr('stroke-width', arrow_slope/3);
     }
 
     function calcX()  {
@@ -175,40 +154,35 @@ function createOccupancyApp() {
       .attr('y', height - line_width*2 - marker_line_height - 5)
       .attr('alignment-baseline', 'middle')
       .attr('text-anchor', 'middle')
-      .style('font-family', font_family)      
+      .style('font-family', font_family)
       .attr('fill', 'black')
       .style('text-transform', 'uppercase')
       .attr('x', calcX);
 
-    if(selected_obj.level != 'stable') {
-      
-     var triangle_width = line_width/2;
-     var triangle_height = triangle_width/2;
-     var number_of_triangles = 3;
-
+    if(selected_obj.level != 'stable') { // Drawing the smaller pointer arrows
      var x_base = ((selected_obj.min - traffic_lights[0].min)/span)*svg_width;
 
     if(selected_obj.level == 'increasing')
-      x_base += ((selected_obj.max - selected_obj.min)/span)*svg_width - (number_of_triangles*(triangle_width+1));
+      x_base += ((selected_obj.max - selected_obj.min)/span)*svg_width - (small_arrows*(small_arrow_width+1));
 
     if(x_base < 0) // Prevent arrows from overflowing to left
-      x_base += (number_of_triangles*(triangle_width+1))/2;
-    
-    if(x_base + (number_of_triangles*(triangle_width+1)) > svg_width) // Prevent arrows from overflowing to right
-      x_base -= (number_of_triangles*(triangle_width+1))/2;
+      x_base += (small_arrows*(small_arrow_width+1))/2;
+
+    if(x_base + (small_arrows*(small_arrow_width+1)) > svg_width) // Prevent arrows from overflowing to right
+      x_base -= (small_arrows*(small_arrow_width+1))/2;
 
      var y_base = height - line_width/2 - marker_line_height;
 
-      for(var i = 0; i < number_of_triangles; i++)  {
-        var x = (x_base + (((selected_obj.level == 'increasing') ? i : i+1)*triangle_width+1));
-        points = '' + x + ', ' + (y_base - triangle_height) // Left x, y
-        points +=  ', ' + (x + ((selected_obj.level == 'increasing') ? triangle_width : -triangle_width)) + ', ' + y_base // Middle x, y
-        points +=  ', ' + x + ', ' + (y_base + triangle_height) // Right x, y
+      for(var i = 0; i < small_arrows; i++)  {
+        var x = (x_base + (((selected_obj.level == 'increasing') ? i : i+1)*small_arrow_width+1));
+        points = '' + x + ', ' + (y_base - small_arrow_height) // Left x, y
+        points +=  ', ' + (x + ((selected_obj.level == 'increasing') ? small_arrow_width : -small_arrow_width)) + ', ' + y_base // Middle x, y
+        points +=  ', ' + x + ', ' + (y_base + small_arrow_height) // Right x, y
         points +=  ', ' + (x + ((selected_obj.level == 'increasing') ? 2 : -2)) + ', ' + y_base // Middle x, y
 
         svg.append('polygon')
           .attr('points', points)
-          .style('fill', 'black')
+          .style('fill', arrow_color)
           .style('stroke', 'white')
           .style('stroke-width', 1)
           .style('cursor', 'help')
@@ -220,12 +194,12 @@ function createOccupancyApp() {
 
       // Text with updated time
       svg.append('text')
-        .text('Updated: ' + obj.time)        
+        .text('Updated: ' + obj.time)
         .attr('y', 16)
         .style('font-family', font_family)
         .style('font-size', '14px')
         .attr('x', function(d) { return (selected_obj.nr == 0) ? svg_width - this.getComputedTextLength(): 0; } )
-     
+
   });
 }
 
